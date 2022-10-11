7443dbafe4ec806168dc7c46e57a84c4735608a0
Chad <chad.cromer@cyclomaticcomplexity.com>
2022-10-10 20:10:07 -0500

    init

---
+++ Source/WebApplication1/WebApplication1/Controllers/RouteController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/routes")]
+    public class RouteController : Controller
+    {
+        [HttpGet("{value}")]
+        [Produces("application/json")]
+        public ActionResult Get(string value)
+        {
+            var result = new
+            {
+                ReceivedValue = value,
+                DecodedValue = HttpUtility.UrlDecode(value)
+            };
+
+            return Ok(result);
+        }
+    }
+}
